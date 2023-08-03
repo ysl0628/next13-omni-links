0bd8ac615effb6a81951d55621967fb85d7213ac
ysl0628 <yihsinlan@gmail.com>
2023-08-03 19:08:31 +0800

    feat: implement UserButtons on NavBar

---
+++ app/components/navbar/UserButtons.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react'
+import Button from '../Button'
+
+const UserButtons = () => {
+  return (
+    <div className="flex flex-[0_0_auto] min-w-0 items-center gap-4">
+      <Button label="Log In" text onClick={() => {}} />
+      <Button label="Sign Up" onClick={() => {}} />
+    </div>
+  )
+}
+
+export default UserButtons
