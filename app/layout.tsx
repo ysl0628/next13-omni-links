c922ed1ec8d417c7197e8634092b1041bd438a86
ysl0628 <yihsinlan@gmail.com>
2023-08-03 21:22:05 +0800

    feat: implement NavBar in home page style

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-import NavBar from './components/navbar/NavBar'
+import NavBar from '../components/navbar/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {/* 放置 Navbar */}
-        <NavBar />
+        <NavBar home />
         {children}
       </body>
     </html>
---
+++ components/navbar/NavBar.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import Logo from './Logo'
+import Container from '../Container'
+import NavButtons from './NavButtons'
+import UserButtons from './UserButtons'
+
+const NavBar = ({ home }: { home?: boolean }) => {
+  return (
+    <div
+      className={`fixed w-full 
+      ${home ? 'bg-[#F7F8F9]' : 'bg-white shadow-sm'} 
+      z-10`}
+    >
+      <div className={`py-4 ${home ? '' : 'border-b-[1px]'} `}>
+        <Container>
+          <div className="flex items-center w-full flex-grow justify-between gap-3 md:gap:0">
+            <Logo />
+            <NavButtons />
+            <UserButtons />
+          </div>
+        </Container>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
