8cef7bcc85de78698e4bf8aa7d8ed4f33db82a5f
ysl0628 <yihsinlan@gmail.com>
2023-08-06 13:28:54 +0800

    feat: implement onClick handler in Logo

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,12 +1,18 @@
+'use client'
+
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import logo from 'public/images/logo.svg'
 
 const Logo = () => {
+  const router = useRouter()
+
   return (
     <Image
       src={logo}
       alt="logo"
-      className="hidden md:block object-cover"
+      className="hidden md:block object-cover cursor-pointer"
+      onClick={() => router.push('/')}
       priority
     />
     // 下方寫法必須加上 width 與 height，或是 fill
