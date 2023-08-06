e2f6977f8cbe82332e350255e3bad8ce796ed9ae
ysl0628 <yihsinlan@gmail.com>
2023-08-06 16:48:26 +0800

    feat: implement page by parallel routes

---
+++ app/@admin/admin/layout.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next'
+
+interface AdminLayoutProps {
+  children: React.ReactNode
+}
+
+export const metadata: Metadata = {
+  title: 'Admin',
+  description: 'Setting Your LinkOrchard'
+}
+
+export default function AdminLayout({ children }: AdminLayoutProps) {
+  return <section className="w-full flex pt-2.5 bg-grey-50">{children}</section>
+}
