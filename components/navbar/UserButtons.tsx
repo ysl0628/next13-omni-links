7564c1de734b6fc2652e9afa7f3f4ade20beea67
ysl0628 <yihsinlan@gmail.com>
2023-08-06 13:34:57 +0800

    feat: implement Sign Up button handler

--- components/navbar/UserButtons.tsx
+++ components/navbar/UserButtons.tsx
@@ -1,17 +1,21 @@
 'use client'
 
 import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
 import Button from '../Button'
 
 const UserButtons = () => {
+  const router = useRouter()
   return (
     <div className="flex flex-[0_0_auto] min-w-0 items-center gap-4">
       <Button
         label="Log In"
         variant="text"
-        onClick={() => console.log('Log In')}
+        onClick={() => router.push('/login')}
       />
-      <Button label="Sign Up" onClick={() => {}} />
+      <Button label="Sign Up" onClick={() => router.push('/signup')} />
     </div>
   )
 }
