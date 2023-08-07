317958de8efdaad2675834447645c5901eccb171
ysl0628 <yihsinlan@gmail.com>
2023-08-07 22:13:05 +0800

    feat: extends size props for Avatar

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -5,15 +5,16 @@ import Image from 'next/image'
 
 interface AvatarProps {
   src?: string | null
+  size?: number
 }
 
-const Avatar: React.FC<AvatarProps> = ({ src }) => {
+const Avatar: React.FC<AvatarProps> = ({ src, size }) => {
   return (
     <Image
       alt="avatar"
       className="rounded-full"
-      height={40}
-      width={40}
+      height={size || 40}
+      width={size || 40}
       src={src || '/images/placeholder.jpg'}
     />
   )
