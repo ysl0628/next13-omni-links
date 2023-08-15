f753cbb4665718ef0b732c7e7a8c761308af0d9b
ysl0628 <yihsinlan@gmail.com>
2023-08-15 23:28:42 +0800

    feat: implement signIn by credentials

--- components/auth/LoginClient.tsx
+++ components/auth/LoginClient.tsx
@@ -1,7 +1,11 @@
 'use client'
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import { useFormik } from 'formik'
+import { signIn } from 'next-auth/react'
+import { toast } from 'react-hot-toast'
+import * as Yup from 'yup'
 
 import Button from '@/components/Button'
 import Input from '@/components/input/Input'
@@ -10,15 +14,44 @@ import { AiFillGithub } from 'react-icons/ai'
 import { BiLogoGoogle, BiLogoFacebook } from 'react-icons/bi'
 
 interface FormikValues {
-  email: string
-  password: string
+  email?: string
+  password?: string
 }
 
 const LoginClient = () => {
-  const formik = useFormik({
+  const router = useRouter()
+  const [isLoading, setIsLoading] = React.useState(false)
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('請輸入正確的 Email')
+      .required('Email 為必填欄位'),
+    password: Yup.string().required('Password 為必填欄位')
+  })
+
+  const formik = useFormik<FormikValues>({
     initialValues: {},
+    validationSchema,
+    validateOnChange: false,
+    validateOnBlur: false,
     onSubmit: (values) => {
-      console.log(values)
+      setIsLoading(true)
+
+      signIn('credentials', {
+        email: values.email,
+        password: values.password,
+        redirect: false
+      }).then((callback) => {
+        setIsLoading(false)
+        if (callback?.ok) {
+          toast.success('登入成功')
+          router.push('/admin')
+        }
+
+        if (callback?.error) {
+          toast.error('登入失敗')
+        }
+      })
     }
   })
 
@@ -28,44 +61,45 @@ const LoginClient = () => {
         <h1 className="text-grey-600 text-3xl font-semibold">Log In</h1>
       </div>
       <div className="flex flex-col gap-6">
-        <Input
-          formik={formik}
-          id="email"
-          name="email"
-          label="Email"
-          className=""
-        />
-        <Input
-          formik={formik}
-          id="password"
-          name="password"
-          label="Password"
-          className=""
-        />
-        <Button label="Log In" onClick={() => {}} className="w-full" />
-        <div className="text-md text-center text-gray-300 font-light">
-          Connect With
-        </div>
-        <div className="flex flex-col gap-2 md:flex-row md:gap-4 justify-center">
-          <Button
-            icon={BiLogoGoogle}
-            onClick={() => {}}
-            className="w-full"
-            color="warning"
+        <form className="contents" onSubmit={formik.handleSubmit}>
+          <Input formik={formik} name="email" label="Email" className="" />
+          <Input
+            formik={formik}
+            name="password"
+            label="Password"
+            type="password"
+            className=""
           />
           <Button
-            icon={BiLogoFacebook}
-            onClick={() => {}}
+            label="Log In"
+            type="submit"
             className="w-full"
-            color="info"
+            disabled={isLoading}
           />
-          <Button
-            icon={AiFillGithub}
-            onClick={() => {}}
-            className="w-full"
-            color="dark"
-          />
-        </div>
+          <div className="text-md text-center text-gray-300 font-light">
+            Connect With
+          </div>
+          <div className="flex flex-col gap-2 md:flex-row md:gap-4 justify-center">
+            <Button
+              icon={BiLogoGoogle}
+              onClick={() => {}}
+              className="w-full"
+              color="warning"
+            />
+            <Button
+              icon={BiLogoFacebook}
+              onClick={() => {}}
+              className="w-full"
+              color="info"
+            />
+            <Button
+              icon={AiFillGithub}
+              onClick={() => {}}
+              className="w-full"
+              color="dark"
+            />
+          </div>
+        </form>
         <div className="text-md text-center text-gray-300 font-light">
           Not a member yet?{' '}
           <Link
