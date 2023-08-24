fc3b3b369f59f01bf5f6ec00d3e261b58bdca607
ysl0628 <yihsinlan@gmail.com>
2023-08-24 23:59:40 +0800

    feat: implement toast provider

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -4,9 +4,15 @@ import React, { FC, ReactNode, useEffect, useState } from 'react'
 import { ThemeProvider } from 'next-themes'
 
 import { type ThemeProviderProps } from 'next-themes/dist/types'
+import ToasterProvider from './ToastProvider'
 
 const Providers: FC<ThemeProviderProps> = ({ children, ...props }) => {
-  return <ThemeProvider {...props}>{children}</ThemeProvider>
+  return (
+    <ThemeProvider {...props}>
+      <ToasterProvider />
+      {children}
+    </ThemeProvider>
+  )
 }
 
 export default Providers
