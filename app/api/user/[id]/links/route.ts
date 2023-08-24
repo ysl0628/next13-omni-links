65b8a85b25b8db5dd10d1b6d061f9ab0ff3348e6
ysl0628 <yihsinlan@gmail.com>
2023-08-24 23:58:53 +0800

    feat: implement order check

--- app/api/user/[id]/links/route.ts
+++ app/api/user/[id]/links/route.ts
@@ -2,6 +2,7 @@ import { getCurrentUser } from '@/actions/getCurrentUser'
 import { NextResponse } from 'next/server'
 
 import prisma from '@/libs/prismadb'
+import { LinkType } from '@prisma/client'
 
 export async function POST(
   req: Request,
@@ -15,6 +16,24 @@ export async function POST(
     const body = await req.json()
 
     const { title, type, url, order } = body
+
+    // 檢查型別
+    if (typeof title !== 'string')
+      return new NextResponse('title must be string ', { status: 400 })
+
+    if (type.id === 'default')
+      return new NextResponse('type must select ', { status: 400 })
+
+    // 檢查 order 有沒有重複
+    const checkOrder = await prisma.link.findFirst({
+      where: {
+        order: order,
+        userId: params.id
+      }
+    })
+
+    if (checkOrder)
+      return new NextResponse('order is duplicate', { status: 400 })
 
     const data = await prisma.link.create({
       data: {
@@ -30,6 +49,6 @@ export async function POST(
 
     return NextResponse.json({ data, message: 'success' })
   } catch (error) {
-    return NextResponse.error()
+    return console.log(error)
   }
 }
