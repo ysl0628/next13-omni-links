0ee4157963756891832c521b79d1a6ce15254cde
ysl0628 <yihsinlan@gmail.com>
2023-08-27 23:56:56 +0800

    feat: implement update admin feature

--- components/portal/BasicSetting.tsx
+++ components/portal/BasicSetting.tsx
@@ -1,4 +1,6 @@
 'use client'
+import axios from 'axios'
+import { toast } from 'react-hot-toast'
 import { useFormik } from 'formik'
 
 import Avatar from '../Avatar'
@@ -39,11 +41,19 @@ interface FormValues {
 
 const BasicSetting: React.FC<BasicSettingProps> = () => {
   const { update, admin } = useSetup((state) => state)
+  const user = useSetup((state) => state.user)
 
   const formik = useFormik<FormValues>({
     initialValues: { ...admin },
-    onSubmit: (values) => {
-      console.log(values)
+    onSubmit: async (values) => {
+      try {
+        const { data: res } = await axios.put(`/api/user/${user?.id}`, values)
+        update({ admin: res.data })
+        toast.success('更新成功')
+      } catch (error) {
+        toast.error('更新失敗')
+        console.log(error)
+      }
     }
   })
 
@@ -67,64 +77,67 @@ const BasicSetting: React.FC<BasicSettingProps> = () => {
         基本設定
       </div>
       <Divider />
-      <div className="px-6 pt-6 pb-4 w-full flex justify-between flex-grow items-center md:flex-row flex-col gap-12">
-        <Avatar size={150} src={avatarImage} />
-        <div className="flex justify-between items-center flex-auto gap-6">
-          <ImageUpload
-            label="上傳圖片"
-            rounded="full"
+      <form className="contents" onSubmit={formik.handleSubmit}>
+        <div className="px-6 pt-6 pb-4 w-full flex justify-between flex-grow items-center md:flex-row flex-col gap-12">
+          <Avatar size={150} src={avatarImage} />
+          <div className="flex justify-between items-center flex-auto gap-6">
+            <ImageUpload
+              label="上傳圖片"
+              rounded="full"
+              size="large"
+              className="w-full"
+              onChange={(url) => handleUpload(url)}
+            />
+            <Button
+              label="重置圖片"
+              variant="outline"
+              className="w-full"
+              size="large"
+              rounded="full"
+              onClick={handleResetImg}
+            />
+          </div>
+        </div>
+        <div className="px-6 py-4 flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <LabelInput
+              formik={formik}
+              name="title"
+              label="標題"
+              placeholder="請輸入標題"
+            />
+            <LabelInput
+              formik={formik}
+              name="description"
+              label="簡介"
+              textarea
+              placeholder="請輸入用簡介"
+            />
+            <ButtonGroup
+              title="主題色"
+              name="themeColor"
+              list={themeList}
+              formik={formik}
+            />
+          </div>
+        </div>
+
+        <div className="px-6 py-4 flex gap-4 justify-end">
+          <Button
+            label="預覽"
+            color="secondary"
             size="large"
-            className="w-full"
-            onChange={(url) => handleUpload(url)}
+            className="w-full md:w-1/4"
+            onClick={handlePreview}
           />
           <Button
-            label="重置圖片"
-            variant="outline"
-            className="w-full"
+            label="儲存"
             size="large"
-            rounded="full"
-            onClick={handleResetImg}
+            className="w-full md:w-1/4"
+            type="submit"
           />
         </div>
-      </div>
-      <div className="px-6 py-4 flex flex-col gap-4">
-        <div className="flex flex-col gap-2">
-          <LabelInput
-            formik={formik}
-            name="title"
-            label="標題"
-            placeholder="請輸入標題"
-          />
-          <LabelInput
-            formik={formik}
-            name="description"
-            label="簡介"
-            textarea
-            placeholder="請輸入用簡介"
-          />
-          <ButtonGroup
-            title="主題色"
-            name="themeColor"
-            list={themeList}
-            formik={formik}
-          />
-        </div>
-      </div>
-      <div className="px-6 py-4 flex gap-4 justify-end">
-        <Button
-          label="預覽"
-          color="secondary"
-          size="large"
-          className="w-full md:w-1/4"
-          onClick={handlePreview}
-        />
-        <Button
-          label="儲存"
-          size="large"
-          className="w-full md:w-1/4"
-          onClick={() => {}}
-        />
-      </div>
+      </form>
     </>
   )
 }
