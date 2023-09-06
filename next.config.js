cad98a5d40cb32e046224bee950500b2f63b0a74
ysl0628 <yihsinlan@gmail.com>
2023-09-06 21:02:36 +0800

    chore: add facebook avatar domain

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: [
       'avatars.githubusercontent.com',
+      'platform-lookaside.fbsbx.com',
       'lh3.googleusercontent.com',
       'res.cloudinary.com'
     ]
