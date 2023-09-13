cd6eac7281b166dfb85f51dde65b36be42fa1993
ysl0628 <yihsinlan@gmail.com>
2023-09-13 19:52:25 +0800

    feat: add account link in menu

--- components/navbar/UserMenu.tsx
+++ components/navbar/UserMenu.tsx
@@ -48,6 +48,10 @@ const UserMenu: React.FC<UserMenuProps> = ({ avatarImage, username }) => {
                 <Divider />
                 <div className="flex flex-col gap-1 pt-2">
                   <MenuItem
+                    label="個人設定"
+                    onClick={() => router.push('/portal/account')}
+                  />
+                  <MenuItem
                     label="開始我的 Link Orchard"
                     onClick={() => {
                       router.push('/portal/basic')
