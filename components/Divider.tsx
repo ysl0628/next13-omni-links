d57242119ca782a19a7c444e262633d06f90258a
ysl0628 <yihsinlan@gmail.com>
2023-09-19 00:30:30 +0800

    feat: extends Divider color

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 
 interface DividerProps {
   vertical?: boolean
+  color?: string
 }
 
-const Divider = ({ vertical }: DividerProps) => {
+const Divider = ({ vertical, color }: DividerProps) => {
+  const bgColor = color ? `bg-${color}` : 'bg-neutral-100'
   return vertical ? (
-    <div className="h-full min-h-[5rem] flex-grow w-0.5 self-stretch bg-neutral-300 opacity-100 dark:opacity-50" />
+    <div
+      className={`h-full min-h-[5rem] flex-grow w-0.5 self-stretch ${bgColor} opacity-100 dark:opacity-50`}
+    />
   ) : (
-    <div className="h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
+    <div
+      className={`h-0.5 border-t-0 ${bgColor} opacity-100 dark:opacity-50`}
+    />
   )
 }
 
