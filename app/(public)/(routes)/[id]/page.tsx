9ab3d5fc55f39b2461c9c5a94d5d9de8fe546e6c
ysl0628 <yihsinlan@gmail.com>
2023-09-21 23:55:53 +0800

    feat: add loading to root and portal

--- app/(public)/(routes)/[id]/page.tsx
+++ app/(public)/(routes)/[id]/page.tsx
@@ -6,6 +6,8 @@ import { getUserByUsername } from '@/actions/getUserByUsername'
 
 import PublicClient from '@/components/page/public/PublicClient'
 
+export const revalidate = 60
+
 const Page = async ({ params }: { params: { id: string } }) => {
   const user = await getUserByUsername(params.id)
   const currentUser = await getCurrentUser()
