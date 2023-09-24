f4e1707368fc8a240868f866a9629cb8d9bb1773
ysl0628 <yihsinlan@gmail.com>
2023-09-25 00:17:21 +0800

    feat: modify page min width

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
     <div className="flex bg-grey-50 w-full">
       <Container>
         <div className="flex justify-center items-center h-[95%]">
-          <div className="w-full p-2 px-16 pb-8 min-w-[32rem] shadow-md rounded bg-white">
+          <div className="w-full p-2 px-16 pb-8 min-w-[20rem] md:min-w-[32rem] shadow-md rounded bg-white">
             <LoginClient />
           </div>
         </div>
