26cc3565372e777934f22924bd75c3ee4fe5dee2
ysl0628 <yihsinlan@gmail.com>
2023-10-15 14:20:16 +0800

    style: modify rwd style

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -6,7 +6,7 @@ export default function SignUpPage() {
     <div className="flex bg-grey-50 w-full">
       <Container>
         <div className="flex justify-center items-center h-[95%]">
-          <div className="w-full p-2 px-16 pb-8 min-w-[20rem] md:min-w-[32rem]] shadow-md rounded bg-white">
+          <div className="w-full p-2 px-16 pb-8 min-w-[20rem] md:max-w-[32rem] md:shadow-md md:rounded md:bg-white bg-grey-50">
             <SignUpClient />
           </div>
         </div>
