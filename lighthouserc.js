c069723927526224b1d824018b09007f47f7bc11
ysl0628 <yihsinlan@gmail.com>
2023-11-02 22:37:47 +0800

    feat: add lighthouse ci yml

---
+++ lighthouserc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  ci: {
+    assert: {
+      collect: {
+        url: process.env.CI
+          ? 'https://link-orchard.vercel.app/'
+          : 'http://localhost:3000',
+        startServerCommand: process.env.CI ? undefined : 'npm run dev',
+        settings: {
+          emulatedFormFactor: 'desktop'
+        }
+      },
+      preset: 'lighthouse:no-pwa',
+      assertions: {
+        'categories:performance': [
+          'warn',
+          { aggregationMethod: 'optimistic', minScore: 0.75 }
+        ],
+        'categories:accessibility': [
+          'error',
+          { aggregationMethod: 'optimistic', minScore: 0.6 }
+        ]
+      },
+      upload: {
+        target: 'temporary-public-storage'
+      }
+    }
+  }
+}
