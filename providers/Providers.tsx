233bde5541d6e656e39e90d51b15649aa5be73a7
ysl0628 <yihsinlan@gmail.com>
2023-12-25 23:24:55 +0800

    feat: add Faro initialization and session tracking

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { ThemeProvider } from 'next-themes'
 
 import { type ThemeProviderProps } from 'next-themes/dist/types'
@@ -8,26 +8,36 @@ import ToasterProvider from './ToastProvider'
 import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk'
 import { TracingInstrumentation } from '@grafana/faro-web-tracing'
 
-const faro = initializeFaro({
-  url: 'https://faro-collector-prod-ap-southeast-1.grafana.net/collect/211d4c70d3ffbc4147c59c2e4631ea84',
-  app: {
-    name: 'linkOrchard',
-    version: '1.0.0',
-    environment: process.env.NODE_ENV
-  },
-  instrumentations: [
-    // Mandatory, overwriting the instrumentations array would cause the default instrumentations to be omitted
-    ...getWebInstrumentations(),
+const faroInit = () => {
+  if (typeof window !== 'undefined') {
+    const faro = initializeFaro({
+      url: 'https://faro-collector-prod-ap-southeast-1.grafana.net/collect/211d4c70d3ffbc4147c59c2e4631ea84',
+      app: {
+        name: 'linkOrchard',
+        version: '1.0.0',
+        environment: process.env.NODE_ENV
+      },
+      sessionTracking: {
+        enabled: true,
+        samplingRate: 0.5
+      },
+      instrumentations: [
+        // Mandatory, overwriting the instrumentations array would cause the default instrumentations to be omitted
+        ...getWebInstrumentations(),
 
-    // Initialization of the tracing package.
-    // This packages is optional because it increases the bundle size noticeably. Only add it if you want tracing data.
-    new TracingInstrumentation()
-  ]
-})
+        // Initialization of the tracing package.
+        // This packages is optional because it increases the bundle size noticeably. Only add it if you want tracing data.
+        new TracingInstrumentation()
+      ]
+    })
+    return faro
+  }
+}
 
 const Providers: FC<ThemeProviderProps> = ({ children, ...props }) => {
-  // const error = new Error('test error')
-  // faro.api.pushError(error)
+  useEffect(() => {
+    faroInit()
+  }, [])
   return (
     <>
       <ToasterProvider />
