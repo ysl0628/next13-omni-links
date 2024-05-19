7eea362f9701bcbef604fb609a0aa0bf5830a4e6
ysl0628 <yihsinlan@gmail.com>
2024-05-19 15:33:58 +0800

    fix: update ButtonLoader color to match design specifications

--- components/ButtonLoader.tsx
+++ components/ButtonLoader.tsx
@@ -5,7 +5,7 @@ import ClipLoader from 'react-spinners/ClipLoader'
 const ButtonLoader = () => {
   return (
     <div className="flex justify-center items-center">
-      <ClipLoader size={20} color="#14532D" />
+      <ClipLoader size={20} color="rgb(245 200 80)" />
     </div>
   )
 }
