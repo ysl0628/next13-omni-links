298f9a3d83fb3cbeb89bbec43d0df30554db7902
ysl0628 <yihsinlan@gmail.com>
2024-09-15 18:38:23 +0800

    fix: modify faro register error

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -1,25 +1,25 @@
-'use client'
+"use client";
 
-import React, { FC, useEffect } from 'react'
-import { ThemeProvider } from 'next-themes'
+import React, { FC } from "react";
+import { ThemeProvider } from "next-themes";
 
-import { type ThemeProviderProps } from 'next-themes/dist/types'
-import ToasterProvider from './ToastProvider'
-import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk'
-import { TracingInstrumentation } from '@grafana/faro-web-tracing'
+import { type ThemeProviderProps } from "next-themes/dist/types";
+import ToasterProvider from "./ToastProvider";
+import { getWebInstrumentations, initializeFaro } from "@grafana/faro-web-sdk";
+import { TracingInstrumentation } from "@grafana/faro-web-tracing";
 
 const faroInit = () => {
-  if (typeof window !== 'undefined') {
+  if (typeof window !== "undefined") {
     const faro = initializeFaro({
-      url: 'https://faro-collector-prod-ap-southeast-1.grafana.net/collect/211d4c70d3ffbc4147c59c2e4631ea84',
+      url: "https://faro-collector-prod-ap-southeast-1.grafana.net/collect/211d4c70d3ffbc4147c59c2e4631ea84",
       app: {
-        name: 'linkOrchard',
-        version: '1.0.0',
-        environment: process.env.NODE_ENV
+        name: "linkOrchard",
+        version: "1.0.0",
+        environment: process.env.NODE_ENV,
       },
       sessionTracking: {
         enabled: true,
-        samplingRate: 0.5
+        samplingRate: 0.5,
       },
       instrumentations: [
         // Mandatory, overwriting the instrumentations array would cause the default instrumentations to be omitted
@@ -27,23 +27,22 @@ const faroInit = () => {
 
         // Initialization of the tracing package.
         // This packages is optional because it increases the bundle size noticeably. Only add it if you want tracing data.
-        new TracingInstrumentation()
-      ]
-    })
-    return faro
+        new TracingInstrumentation(),
+      ],
+    });
+    return faro;
   }
-}
+};
+
+faroInit();
 
 const Providers: FC<ThemeProviderProps> = ({ children, ...props }) => {
-  useEffect(() => {
-    faroInit()
-  }, [])
   return (
     <>
       <ToasterProvider />
       {children}
     </>
-  )
-}
+  );
+};
 
-export default Providers
+export default Providers;
